1b3ffad96f0dc800912b17956e95b6e84117ffff
Marco Zazzini <47858428+MarcoZazzini1989@users.noreply.github.com>
2024-06-06 10:12:02 +0000

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <div>
+    <button v-on:click="count++">Click {{ count }}</button>
+  </div>
+</template>
