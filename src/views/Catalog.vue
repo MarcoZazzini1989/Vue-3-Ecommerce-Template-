b2149c8ee14b3fd88900971c8986ddc1a6257438
Marco Zazzini <47858428+MarcoZazzini1989@users.noreply.github.com>
2024-06-07 01:34:22 +0000

    added homepage catalog with css

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -6,25 +6,44 @@ import { productsStore } from "../stores/products.js"
 const store = productsStore()
 
 
-onMounted(() => {
-  console.log(`the component is now mounted.`)
-  store.fetchProductsFromDB()
-})
+  onMounted( async  () => {
+             await store.fetchProductsFromDB()
+             console.log(`the component is now mounted.`)
+            })
 
 
 
 </script>
 
 <template>
-  <div>
-    <h1>Catalog</h1>
-    <prova />
-<div class="products" v-for="product in productsStore.products" :key="product.id">
+<div class="product-list">
+<!---<h1>Catalog</h1>-->   
+<div class="product" v-for="product in store.products" :key="product.id">
     <img :src="product.thumbnail"      > </img>
     <h2> Brand : {{ product.brand  }}  </h2>
-
+    <p>Description : {{product.description}}</p>
+    <p>Price : ${{product.price}}</p>
 </div>
-
-
-  </div>
+</div>
 </template>
+
+<style scoped>
+.product-list {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  cursor:pointer;
+}
+
+.product {
+  flex-basis: 28%;
+  margin: 8px;
+  padding: 16px;
+  box-shadow: 0px 0px 14px 1px #e6e6e6;
+}
+
+.product image {
+  width:70%;
+}
+
+</style>
