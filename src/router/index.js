7ec36d2d1c98d5d4acff183544b8c0bb0cf0d098
Marco Zazzini <47858428+MarcoZazzini1989@users.noreply.github.com>
2024-06-07 01:42:48 +0000

    dettagli prodotto

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from "../views/Home.vue"
-import Catalog from "../views/Catalog.vue"
-
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Catalog from "../views/Catalog.vue";
+import ProductDetails from "../views/ProductDEtails.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{
-    path: '/',
-    name: 'Home',
-    component: Catalog
-  }],
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Catalog,
+    },
+    {
+      path: "/product/:id",
+      name: "Productview",
+      component: ProductDetails,
+    },
+  ],
 });
 
-export default router
+export default router;
